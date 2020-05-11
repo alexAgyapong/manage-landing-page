a9377e6866a458e2d4d2422072f144997e9c39e4
Alexander Agyapong <30531859+alexAgyapong@users.noreply.github.com>
2020-05-11 08:48:00 +0100

    Started styling for mobile design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { ManageLandingPageComponent } from './manage-landing-page/manage-landing-page.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [{ path: '', component: ManageLandingPageComponent },];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ManageLandingPageComponent } from './manage-landing-page/manage-landing-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ManageLandingPageComponent
   ],
   imports: [
     BrowserModule,

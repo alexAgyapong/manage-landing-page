4225219427e3a8b8fbe4d9cc23ab77879fe44855
Alexander Agyapong <30531859+alexAgyapong@users.noreply.github.com>
2020-05-11 21:44:35 +0100

    Add testimonials and footer background colour

--- src/app/manage-landing-page/manage-landing-page.component.ts
+++ src/app/manage-landing-page/manage-landing-page.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./manage-landing-page.component.css']
 })
 export class ManageLandingPageComponent implements OnInit {
+  itemsPerSlide = 3;
+  singleSlideOffset = true;
+  noWrap = true;
 
+  slides = [
+    {image: 'assets/images/avatar-shanai.png'},
+    {image: 'assets/images/avatar-ali.png'},
+    {image: 'assets/images/avatar-richard.png'},
+    {image: 'assets/images/avatar-richard.png'},
+    {image: 'assets/images/avatar-richard.png'},
+    {image: 'assets/images/avatar-ali.png'},
+    {image: 'assets/images/avatar-shanai.png'},
+    {image: 'assets/images/avatar-richard.png'},
+    {image: 'assets/images/avatar-anisha.png'},
+  ];
   constructor() { }
 
   ngOnInit() {
+
   }
 
 }
